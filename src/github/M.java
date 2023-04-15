76ed66edc64bcdd77428287b4e0ebff10cf98ac6
Manish Jha <jham73402@gmail.com>
2023-04-14 21:23:34 -0800

    this is my project

---
+++ src/github/M.java
@@ -0,0 +1,10 @@
+package github;
+
+public class M {
+    public static void main(String []djd){
+        int a = 5;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
